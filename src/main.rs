d948fe2e149ee2fa85fb43b850fb34724fb6df7c
K <redknclarke@gmail.com>
2024-09-24 11:57:11 -0400

    implemented new params and handled params better implemented -i and -h, added space for -x, used a more generalized method to recieve - paramaters, forgot to add comments (ill do it later)

--- src/main.rs
+++ src/main.rs
@@ -8,65 +8,135 @@ struct Cli {
 }
 
 fn main() -> Result<()> {
-    let search_pattern = std::env::args().nth(1).expect("No Pattern Given");
-    let file_path = std::env::args().nth(2).expect("No Path Given");
-    let params = match std::env::args().nth(3) {
-        Some(p) => p,
-        None => String::new(),
-    };
+    let mut args = std::env::args().skip(1);
+
+    let mut pattern: String = String::new();
+    let mut file_path: Option<std::path::PathBuf> = None;
+    let mut params: String = String::new();
+
+    while let Some(arg) = args.next() {
+        match &arg[..] {
+            "-h" | "--help" => params += "help ",
+            "-r" | "--recursive" => params += "recursive ",
+            "-i" | "--ignore-case" => params += "insensitive ",
+            "-x" | "--regex" => params += "regex ",
+
+            _ => {
+                if arg.starts_with("-") {
+                    println!("Unknown argument '{}'", arg);
+                } else if pattern == "" {
+                    pattern = arg;
+                } else if file_path.is_none() {
+                    let path_arg = std::path::PathBuf::from(arg);
+                    if path_arg.exists() {
+                        file_path = Some(path_arg);
+                    } else {
+                        println!("'{}' no such file or directory", path_arg.display());
+                    }
+                }
+            }
+        }
+    }
+
+    if params.contains("help") {
+        println!("Usage: grop [PATTERN] [FILE] [PARAMS]");
+        println!("Search for PATTERN in each FILE");
+        println!("Example: grop \"Hello World\" src/main.rs -i\n");
+        println!("-h, --help\t\t Show the help screen");
+        println!("-r, --recursive\t\tRecursively search FILE as a directory");
+        println!("-i, --ignore-case\t\tMatch pattern in any case");
+        println!("-x, --regex\t\tInterpret pattern as a regular expression");
+        return Ok(());
+    }
+
+    if params.contains("regex") {
+        println!("Regex not implemented yet");
+        return Ok(());
+    }
+
+    let file_path = file_path.get_or_insert(std::path::PathBuf::from("."));
 
     let args = Cli {
-        pattern: search_pattern,
-        path: std::path::PathBuf::from(file_path),
+        pattern: pattern,
+        path: file_path.to_owned(),
         params: params,
     };
 
-    if args.params.contains("r") {
-        recursive_search(&args.pattern, &args.path).unwrap();
+    if args.params.contains("recursive") {
+        recursive_search(&args).unwrap();
     } else {
         if args.path.is_file() {
-            match search_file(&args.pattern, &args.path) {
+            match search_file(&args) {
                 Err(e) => println!("{}", e),
                 _ => (),
             }
+        } else if args.path.is_dir() {
+            println!("'{}' is a directory", args.path.display());
         } else {
-            println!("No such file {}", args.path.display());
+            println!("no such file {}", args.path.display());
         }
     }
 
     Ok(())
 }
 
-fn search_file(pattern: &str, path: &std::path::PathBuf) -> Result<()> {
-    let contents = std::fs::read_to_string(path)
-        .with_context(|| format!("could not read file `{}`", path.display()))?;
+fn search_file(args: &Cli) -> Result<()> {
+    let contents = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
     for line in contents.lines() {
-        if line.contains(pattern) {
-            print!(
-                "{}{}",
-                path.display().to_string().bright_magenta(),
-                ": ".bright_cyan()
-            );
-            println!(
-                "{}",
-                line.replace(pattern, &pattern.bright_red().bold().to_string())
-            );
+        if args.params.contains("insensitive") {
+            if line.to_lowercase().contains(&args.pattern.to_lowercase()) {
+                print!(
+                    "{}{}",
+                    args.path.display().to_string().bright_magenta(),
+                    ": ".bright_cyan()
+                );
+                println!(
+                    "{}",
+                    line.replace(
+                        &args.pattern.to_lowercase(),
+                        &args.pattern.to_lowercase().bright_red().bold().to_string()
+                    )
+                    .replace(
+                        &args.pattern.to_uppercase(),
+                        &args.pattern.to_uppercase().bright_red().bold().to_string()
+                    )
+                );
+            }
+        } else {
+            if line.contains(&args.pattern) {
+                print!(
+                    "{}{}",
+                    args.path.display().to_string().bright_magenta(),
+                    ": ".bright_cyan()
+                );
+                println!(
+                    "{}",
+                    line.replace(&args.pattern, &args.pattern.bright_red().bold().to_string())
+                );
+            }
         }
     }
     Ok(())
 }
 
-fn recursive_search(pattern: &str, path: &std::path::PathBuf) -> Result<()> {
-    if path.is_file() {
-        match search_file(pattern, path) {
+fn recursive_search(args: &Cli) -> Result<()> {
+    if args.path.is_file() {
+        match search_file(args) {
             // Err(e) => eprintln!("{}", e),
             _ => (),
         }
-    } else if path.is_dir() {
-        for file in std::fs::read_dir(path).unwrap() {
+    } else if args.path.is_dir() {
+        for file in std::fs::read_dir(&args.path).unwrap() {
             let file_path = file.unwrap().path();
-            match recursive_search(&pattern, &file_path) {
+            let new_args = Cli {
+                pattern: args.pattern.to_owned(),
+                path: file_path,
+                params: args.params.to_owned(),
+            };
+
+            match recursive_search(&new_args) {
                 // Err(e) => eprintln!("{}", e),
                 _ => (),
             }
